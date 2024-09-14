ce869e94c573b5b695269029762b369863ef5878
argtok03 <argislitokaj2002@gmail.com>
2024-09-14 23:30:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("img");
+  
+    // List of your image file names stored in the "images" folder
+    const imageFiles = [
+      "pvc_pur_1.jpg",
+      "pvc_pur_2.jpg",
+      "pvc_pur_3.jpg",
+      "gearless1.jpg",
+      "gearless2.jpg"
+    ];
+  
+    // Loop through each <img> element on the page and assign an image from the array
+    let i = 0;
+    for (const image of images) {
+      if (imageFiles[i]) {
+        image.src = `images/${imageFiles[i]}`;  // Set the image source
+        i++;
+      }
+    }
+  });
+  
