6698b870ef7c2267e3c6701e753564a295c6f192
argtok03 <argislitokaj2002@gmail.com>
2024-09-15 01:58:49 +0100

    albania latest

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // List of your image file names stored in the "images" folder
   const imageFiles = [
-    "pvc_pur_1.jpg",
-    "pvc_pur_2.jpg",
-    "pvc_pur_3.jpg",
-    "gearless1.jpg",
-    "gearless2.jpg"
+    "albania_beach.jpg",         // For beach in Tourism section
+    "albanian_alps.jpg",         // For Albanian Alps in Tourism section
+    "tirana.jpg",                // For Tirana city in Cities to Visit
+    "shkodra.jpg",               // For Shkodër city in Cities to Visit
+    "korce.jpg",                 // For Korçë city in Cities to Visit
+    "durres_marina.jpg",         // For Durres Marina in Future section
+    "vlora_marina.jpg"           // For Vlora Marina in Future section
   ];
 
   // Loop through each <img> element on the page and assign an image from the array
   let i = 0;
   for (const image of images) {
     if (imageFiles[i]) {
-      image.src = images/${imageFiles[i]};  // Set the image source
+      image.src = `images/${imageFiles[i]}`;  // Set the image source
       i++;
     }
   }
